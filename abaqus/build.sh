cf353383c58dbe8ec44c022d5f0aabed33934be9
janga1997 <janga1997@localhost.localdomain>
2018-02-03 03:26:26 +0530

    minify files

---
+++ abaqus/build.sh
@@ -0,0 +1,10 @@
+## declare an array variable
+directory='js/custom/'
+declare -a arr=("load" "abaqus" "mitchell")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   uglifyjs "$directory$i.js" > "$directory$i.min.js"
+   # or do whatever with individual element of the array
+done
